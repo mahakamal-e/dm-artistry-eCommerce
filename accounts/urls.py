510e346e89c5067c650a491535415bf9408c4450
mahakamal-e <mahakamal09@gmail.com>
2024-09-10 01:16:09 +0000

    Update settings for authentication URLs and redirection

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,8 +5,6 @@ from shop.views import  profile
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('login/', views.my_login, name='login'),
-    path('profile/', profile, name='profile'),
-    path('logout/', views.custom_logout, name='logout'),
+
    
 ]
