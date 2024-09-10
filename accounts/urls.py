0a4ba43359a94afccfc043301c840f47e528bf52
mahakamal <mahakamal09@gmail.com>
2024-09-10 23:57:08 +0300

    Update URL configuration to include registration view in accounts app

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
-
+from shop.views import  profile
 
 
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.my_login, name='login'),
-    
+    path('profile/', profile, name='profile'),
     path('logout/', views.custom_logout, name='logout'),
+   
 ]
