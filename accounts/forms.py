8400d917dd1915cb83ea536f83c01052329ba995
mahakamal <mahakamal09@gmail.com>
2024-09-10 23:53:21 +0300

    Create custom user registration form & handle errors in form

--- accounts/forms.py
+++ accounts/forms.py
@@ -71,3 +71,13 @@ class CreateUserForm(UserCreationForm):
         if password1 and password2 and password1 != password2:
             raise forms.ValidationError("Passwords don't match")
         return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+    
+
