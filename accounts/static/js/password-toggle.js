870e09214540ba84ddc4824042dc72c1622ae6d7
mahakamal <mahakamal09@gmail.com>
2024-09-11 03:43:47 +0300

    Add userprofile in admin dashboard, move JS to separate file & fix visibility toggle

---
+++ accounts/static/js/password-toggle.js
@@ -0,0 +1,8 @@
+function togglePasswordForm() {
+    var form = document.getElementById('passwordChangeForm');
+    if (form.style.display === "none") {
+        form.style.display = "block";
+    } else {
+        form.style.display = "none";
+    }
+}
