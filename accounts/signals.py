e6621406d76e81fde86afeff9d763b5b71ffae37
mahakamal <mahakamal09@gmail.com>
2024-09-11 04:03:09 +0300

    edit in file signals to solve the proble in user profile

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,13 +1,10 @@
-"""
-This code ensures that every time a new User is created,
-a corresponding UserProfile is also created
-"""
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import UserProfile
 
-@receiver(post_save, sender=User, dispatch_uid='save_new_user_profile')
-def save_profile(sender, instance, created, **kwargs):
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        UserProfile.objects.create(user=instance)
+        UserProfile.objects.create(user=instance)  # Create UserProfile when a new User is created
+
