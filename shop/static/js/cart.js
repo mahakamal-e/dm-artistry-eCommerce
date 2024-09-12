be3db18aee0c6b81d18a73ac1f9d8cbffffdee05
mahakamal-e <mahakamal09@gmail.com>
2024-09-12 18:12:42 +0300

    hanle issue /cart_count/ not found

--- shop/static/js/cart.js
+++ shop/static/js/cart.js
@@ -1,23 +1,3 @@
-// static/js/cart.js
 
-document.addEventListener('DOMContentLoaded', function() {
-    function updateCartCount() {
-        fetch('/cart_count/')
-            .then(response => response.json())
-            .then(data => {
-                document.querySelector('.count-badge').textContent = data.count || 0;
-            });
-    }
-
-    // Call this function initially and after adding/removing items
-    updateCartCount();
-
-    // Example event listener for adding items (you can adapt this to your actual add-to-cart logic)
-    document.querySelectorAll('.add-to-cart-button').forEach(button => {
-        button.addEventListener('click', () => {
-            updateCartCount();
-        });
-    });
-});
 
 
