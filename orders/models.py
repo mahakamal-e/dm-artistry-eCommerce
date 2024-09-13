7b4314f4bfff4646ef7597ba3c503d63b12be387
mahakamal-e <mahakamal09@gmail.com>
2024-09-14 01:42:56 +0300

    add documentation description for models and organize the code

--- orders/models.py
+++ orders/models.py
@@ -1,11 +1,11 @@
-# orders/models.py
-
 from django.contrib.auth.models import User
 from shop.models import Product
 from django.db import models
 from django.utils import timezone
 
+
 class Order(models.Model):
+    """Represents an order placed by a user or a customer"""
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
     phone_number = models.CharField(max_length=20)
     address_line1 = models.CharField(max_length=255)
@@ -13,18 +13,23 @@ class Order(models.Model):
     postcode = models.CharField(max_length=20)
     state = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
-    first_name = models.CharField(max_length=50, default='')  # Set default=''
-    last_name = models.CharField(max_length=50, default='')   # Optionally, you might also want to do the same for last_name
+    first_name = models.CharField(max_length=50, default='')  
+    last_name = models.CharField(max_length=50, default='')
     email = models.EmailField(null=True, blank=True)
-    order_number = models.CharField(max_length=10, blank=True, unique=True, editable=False)  # Added order_number field
+    order_number = models.CharField(max_length=10, blank=True,
+                                    unique=True, editable=False)
     created_at = models.DateTimeField(default=timezone.now)
     
     def save(self, *args, **kwargs):
+        """
+        Override the save method to generate an order number if it is not set
+        """
         if not self.order_number:
             self.order_number = self.generate_order_number()
         super().save(*args, **kwargs)
 
     def generate_order_number(self):
+        """Generate a unique order number."""
         import uuid
         return str(uuid.uuid4().hex[:8].upper())
 
@@ -32,7 +37,13 @@ class Order(models.Model):
         order_info = f"Order {self.order_number}"
         user_info = f"by {self.user.username if self.user else 'Customer'}"
         return f"{order_info} {user_info}"
+ 
+  
 class OrderItem(models.Model):
+    """
+    Represents an item within an order.
+    order has how many items
+    """
     order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
