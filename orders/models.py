fa74b34d37bed44ef17f765ca35f5d744a1b3117
mahakamal-e <mahakamal09@gmail.com>
2024-09-13 15:56:30 +0300

    create Order table Represents a single order placed by a user

---
+++ orders/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+from shop.models import Product
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"Order {self.id} by {self.user.username if self.user else 'Guest'}"
+
