22db3699703f6a2885861f3f85449013ae09972c
mahakamal-e <mahakamal09@gmail.com>
2024-09-14 02:51:25 +0300

    make changes in name url in cart & handle urls in main app

--- cart/urls.py
+++ cart/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
 
-    path('cart/', views.view_cart, name='view_cart'),
+    path('view/', views.view_cart, name='view_cart'),
     path('add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('remove/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
      # For logged-in users
