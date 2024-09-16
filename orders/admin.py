ff57a8a3b2f58e7fe6fe9bd979ce9d3b9c67265f
mahakamal-e <mahakamal09@gmail.com>
2024-09-16 18:46:34 +0300

    handle dublicated in define orders to show in admin dashboard

--- orders/admin.py
+++ orders/admin.py
@@ -3,27 +3,16 @@ from .models import Order, OrderItem
 
 
 class OrderItemInline(admin.TabularInline):
+    """Inline admin interface for editing `OrderItem` instances"""
     model = OrderItem
     extra = 0
     readonly_fields = ('product', 'quantity', 'price')
-
-class OrderAdmin(admin.ModelAdmin):
-    list_display = ('order_number', 'get_user', 'created_at', 'payment_method', 'phone_number', 'first_name', 'last_name', 'email')
-    list_filter = ('created_at', 'payment_method', 'user')  # Add 'user' to filter by registered/guest orders
-    search_fields = ('order_number', 'user__username', 'email', 'first_name', 'last_name')
-    inlines = [OrderItemInline]
-    readonly_fields = ('order_number', 'created_at')
-
-    def get_user(self, obj):
-        return obj.user.username if obj.user else "Guest User"
-    get_user.short_description = 'User'
-    
-
+    # The button to add a new item is provided automatically
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('order_number', 'get_user', 'created_at', 'payment_method', 'phone_number', 'first_name', 'last_name', 'email')
-    list_filter = ('created_at', 'payment_method')
+    list_filter = ('created_at', 'payment_method', 'user')  # Add 'user' to filter by registered/guest orders
     search_fields = ('order_number', 'user__username', 'email', 'first_name', 'last_name')
     inlines = [OrderItemInline]
     readonly_fields = ('order_number', 'created_at')
@@ -36,7 +25,7 @@ class OrderAdmin(admin.ModelAdmin):
     def has_delete_permission(self, request, obj=None):
         return False
 
-@admin.register(OrderItem)
+
 class OrderItemAdmin(admin.ModelAdmin):
     list_display = ('order', 'product', 'quantity', 'price')
     readonly_fields = ('order', 'product', 'quantity', 'price')
