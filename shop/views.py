bdcb3f672611216115ebb84fb3934ca2162f66b0
mahakamal-e <mahakamal09@gmail.com>
2024-09-18 02:42:39 +0000

    add contact form handling to contact_us view- Added logic to handle POST requests for contact form submissions. - Validates form data and displays a success message upon successful submission. - Resets the form after successful submission to clear input fields. - Renders the form and any success messages in the contact_us template.

--- shop/views.py
+++ shop/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product, Category
-from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-from django.contrib.auth import update_session_auth_hash
-from django.contrib import messages
-from django.http import JsonResponse
-from django.db import transaction
-from django.contrib.auth.models import AnonymousUser
+from .forms import ContactForm
+import logging
+
+logger = logging.getLogger(__name__)
 
 def product_list(request):
     products = Product.objects.all()
@@ -50,3 +48,30 @@ def home_view(request):
 
 def about(request):
     return render(request, 'shop/about.html')
+
+
+
+def contact_us(request):
+    success_message = None
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            # Extract form data
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            
+            # Handle form data (e.g., save to database or send email)
+            
+            # Set success message to be displayed in the same template
+            success_message = "Thank you for your message. We will get back to you soon."
+            form = ContactForm()  # Reinitialize form to clear data
+    else:
+        form = ContactForm()
+    
+    context = {
+        'form': form,
+        'success_message': success_message
+        }
+    
+    return render(request, 'shop/contact_us.html', context )
