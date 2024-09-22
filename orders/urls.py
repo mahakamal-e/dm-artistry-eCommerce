ea16418c42be56837975895a1b8ad0eca5bd9b26
mahakamal-e <mahakamal09@gmail.com>
2024-09-22 18:52:01 +0300

    create template for order detail and connect in with profile

--- orders/urls.py
+++ orders/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('checkout/', views.checkout, name='checkout'),
     path('checkout_anonymous/', views.checkout_anonymous, name='checkout_anonymous'),
     path('order_confirmation/<str:order_number>/', views.order_confirmation, name='order_confirmation'),
+    path('order/<str:order_number>/', views.order_detail, name='order_detail'),
 ]
